c1761801701b71a7de044c1b6be3589461b8b76e
quxly4 <quely004@gmail.com>
2025-03-08 22:23:10 -0600

    ...

--- El_Triangulito/Program.cs
+++ El_Triangulito/Program.cs
@@ -6,9 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<elTriangulitoDatabase>(options =>
+builder.Services.AddDbContext<elTriangulitoDBcontex>(options =>
         options.UseSqlServer(
-                builder.Configuration.GetConnectionString("DulceSavorDbConnection")
+                builder.Configuration.GetConnectionString("elTriangulitoDatabase")
             )
         );
 
